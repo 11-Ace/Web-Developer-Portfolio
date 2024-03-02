67836460bbb5ce58fec84877e924981dd47bbb00
Richard Odapu <richardodapu@gmail.com>
2024-03-02 14:43:24 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{html,js}'],
+  theme: {
+    screens: {
+      xl: { max: '1440px' },
+      lg: { max: '976px' },
+      md: { max: '768px' },
+      sm: { max: '480px' },
+    },
+    extend: {
+      colors: {
+        mainBg: 'hsl(240, 5%, 4%)',
+        grayBg: 'hsl(228, 10%, 20%)',
+        headingText: 'hsl(0, 0%, 100%)',
+        primaryText: 'hsl(240, 0%, 65%)',
+        contactLink: 'hsl(53, 96%, 65%)',
+      },
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        prata: ['Prata', 'sans-serif'],
+      },
+      fontSize: {
+        h1: '2.5rem',
+        h2: '1.875rem',
+        h3: '1.5rem',
+      },
+    },
+  },
+  plugins: [],
+};
